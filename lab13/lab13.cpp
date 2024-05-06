a5a18b57dd7e76ef57da33f790d34192f71b4f43
grush <grush@DESKTOP-P5A2MM5>
2024-05-06 12:55:15 +0300

    Добавьте файлы проекта.

---
+++ lab13/lab13.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+double findNthTerm(int n) {
+
+    if (n == 1) {
+        return 0.4;
+    }
+
+    else {
+        return findNthTerm(n - 1) / 10;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter the value of n: ";
+    cin >> n;
+
+
+    cout << "The " << n << "-th term of the series is: " << findNthTerm(n) << endl;
+
+    return 0;
+}
